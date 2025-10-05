2f1c3a8b351c0490d8af26d7ecdd2b3eb26c620b
sean2474 <47488868+sean2474@users.noreply.github.com>
2025-10-05 02:11:31 -0400

    add pages

---
+++ apps/web/src/app/how-to-play/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>How to Play</h1>
+    </div>
+  )
+}
---
+++ apps/web/src/app/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+    </div>
+  )
+}
